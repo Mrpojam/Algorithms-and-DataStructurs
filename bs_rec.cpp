d262c54a148ed1da9d8456ab1506d5cbb93f2b70
Pouya Jafari <70007473+Mrpojam@users.noreply.github.com>
2021-03-14 12:30:45 +0330

    Update bs_rec.cpp

--- bs_rec.cpp
+++ bs_rec.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int a[] = {1, 3, 10, 12, 32, 41, 59, 100};
 
 int bs (int value, int L, int R) {
-	if (R - L <= 1) return L;
+	if (R - L <= 1) return -1;
 	int mid = (L + R) / 2;
 	if (a[mid] == value) return mid;
 	if (a[mid] < value)
